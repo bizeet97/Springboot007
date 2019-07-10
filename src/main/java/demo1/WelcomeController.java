d7bf74cd06b578e694a25eb9a83ade1584b7b794
bizeet97 <bizeet97@gmail.com>
2019-07-10 17:44:55 +0530

    First commit

---
+++ src/main/java/demo1/WelcomeController.java
@@ -0,0 +1,25 @@
+package demo1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@Autowired 
+	private welcomeservice service;
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return service.welcomeretrieve();
+	}
+
+}
+@Component
+class welcomeservice
+{
+	public String welcomeretrieve()
+	{
+		return "Bizeet";
+	}
+}
